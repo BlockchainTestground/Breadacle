37c8fe1267a8dc484e43cbff4a672e8ea6e9faf6
turupawn <ahmed.hn.43@gmail.com>
2021-05-08 18:18:20 -0600

    cleanup

--- Readme.md
+++ Readme.md
@@ -18,3 +18,20 @@ Create a new migration on the `./migrations/` directory following the same seque
 truffle migrate --network mumbai
 truffle migrate --network matic
 ```
+
+# Related contracts and documentation
+
+This project uses a [ERC20 Token](https://github.com/BlockchainTestground/MaticERC20) deployed at `0xd6d897b8FBF9b82c493dc256A2924bE41efda34f` in the mumbai network.
+
+This also uses the [Link Token Contract](https://docs.chain.link/docs/link-token-contracts/) and [Link VRF Contract](https://docs.chain.link/docs/vrf-contracts/).
+
+* Matic:
+  * Link Token: `0xb0897686c545045aFc77CF20eC7A532E3120E0F1`
+  * VRF Coordinator: `0x3d2341ADb2D31f1c5530cDC622016af293177AE0`
+  * Key Hash: `0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da`
+  * Fee: `0.0001 LINK`
+* Mumbai: 
+  * Link Token: `0x326C977E6efc84E512bB9C30f76E30c160eD06FB`
+  * VRF Coordinator: `0x8C7382F9D8f56b33781fE506E897a4F1e2d17255`
+  * Key Hash: `0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4`
+  * Fee: `0.0001 LINK`
--- contracts/TestgroundGame.sol
+++ contracts/TestgroundGame.sol
@@ -6,51 +6,35 @@ import "./dependencies/VRFConsumerBase.sol";
 
 contract TestgroundGame is VRFConsumerBase {
   MyERC20 testground_token = MyERC20(0xd6d897b8FBF9b82c493dc256A2924bE41efda34f);
+  uint256 roll_price = 5e18;
+  uint256 prize = 30e18;
 
-
-
-
-
-
-
-
-
+  // Chainlink internal setup
   bytes32 internal keyHash;
   uint256 internal fee;
 
+  // Random handlers
   mapping(bytes32 => address) public requestIdToMsgSender;
   mapping(address => uint256) public msgSenderToResult;
   mapping(bytes32 => uint256) public requestIdToRandomNumber;
 
-
-
   constructor()
   public
-  VRFConsumerBase(0x8C7382F9D8f56b33781fE506E897a4F1e2d17255, 0x326C977E6efc84E512bB9C30f76E30c160eD06FB)
+  VRFConsumerBase(
+    0x8C7382F9D8f56b33781fE506E897a4F1e2d17255 /* VRF Coordinator */,
+    0x326C977E6efc84E512bB9C30f76E30c160eD06FB /* Link Mumbai Token Contract */)
   {
     keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
     fee = 0.0001 * 10 ** 18;
   }
-
-  function approvee() public returns (bytes32 _requestId) {
-    testground_token.approve(address(this), 500000000000000000000000);
-  }
-
-    function approveOtherContractt(address recipient) public {
-      IERC20 token = MyERC20(0xd6d897b8FBF9b82c493dc256A2924bE41efda34f);
-      token.approve(recipient, 100e18);
-    }
 
   function roll(uint256 userProvidedSeed) public returns (bytes32 _requestId) {
     require(LINK.balanceOf(address(this)) > fee, "Not enough LINK - fill contract with faucet");
     bytes32 requestId = requestRandomness(keyHash, fee, userProvidedSeed);
     requestIdToMsgSender[requestId] = msg.sender;
 
-
-    uint256 roll_price = 5000000000000000000;
     testground_token.approve(address(this), roll_price * 10);
     testground_token.transferFrom(msg.sender, address(this), roll_price);
-
 
     return requestId;
   }
@@ -60,8 +44,7 @@ contract TestgroundGame is VRFConsumerBase {
     msgSenderToResult[requestIdToMsgSender[requestId]] = randomness%6;
     if(randomness%6==5)
     {
-      uint256 prize = 30000000000000000000;
-      testground_token.transferFrom(address(this), requestIdToMsgSender[requestId], prize);
+      testground_token.transferFrom(address(this), requestIdToMsgSender[requestId], win_prize);
     }
   }
 }
